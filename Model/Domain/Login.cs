81c676604715940223adb37841a507f560fb6897
Emilia <emygreat1912@yahoo.com>
2023-02-25 12:31:47 -0700

    feat: added codes for login

--- Model/Domain/Login.cs
+++ Model/Domain/Login.cs
@@ -1,6 +1,24 @@
-﻿namespace MOYO_Website.Model.Domain
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace MOYO_Website.Model.Domain
 {
     public class Login
     {
+        [Required]
+        [StringLength(50)]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+        public int failedLoginAttempts { get; set; }
+
+        public int status { get; set; }
+
+        [Column(TypeName = "date")]
+        public DateTime createDate { get; set; }
     }
 }
--- Technical Services/UtilityClass.cs
+++ Technical Services/UtilityClass.cs
@@ -2,12 +2,33 @@
 using System.Security.Cryptography;
 using System.Text;
 using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.Configuration;
 
 namespace MOYO_Website.Technical_Services
 {
     public class UtilityClass
     {
         private static readonly SHA1Managed Hasher = new SHA1Managed();
+        private static SqlConnection moyoConnection;
+
+        public static SqlConnection getmoyoConnectionString()
+        {
+            var jsonSource = new JsonConfigurationSource()
+            {
+                Path = "appsettings.json"
+            };
+            var builder = new ConfigurationBuilder().Add(jsonSource);
+            var cfg = builder.Build();
+            string dbConnectionString = cfg.GetSection("ConnectionStrings:MoyoConnectionString").Value;
+            moyoConnection = new SqlConnection()
+            {
+                ConnectionString = dbConnectionString
+            };
+            return moyoConnection;
+        }
+
 
         public static string HashToken(string token)
         {
@@ -19,7 +40,7 @@ namespace MOYO_Website.Technical_Services
             // convert hash to string
             return Convert.ToBase64String(passwordHash, 0, passwordHash.Length);
         }
-        public string Encrypt(string input, string password)
+        public static string Encrypt(string input, string password)
         {
             string result = string.Empty;
             byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(input);
@@ -53,7 +74,25 @@ namespace MOYO_Website.Technical_Services
             result = Convert.ToBase64String(encryptedBytes);
             return result;
         }
-        public byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
+
+        public static string EncryptText(string input, string password)
+        {
+            // Get the bytes of the string
+            byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(input);
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Hash the password with SHA256
+            bytesToBeEncrypted = SHA256.Create().ComputeHash(bytesToBeEncrypted);
+            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
+
+            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
+
+            string result = Convert.ToBase64String(bytesEncrypted);
+
+            return result;
+        }
+
+        public static byte[] AES_Encrypt(byte[] bytesToBeEncrypted, byte[] passwordBytes)
         {
             byte[] encryptedBytes = null;
             byte[] saltBytes = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
@@ -82,21 +121,6 @@ namespace MOYO_Website.Technical_Services
             return encryptedBytes;
         }
 
-        public string EncryptText(string input, string password)
-        {
-            // Get the bytes of the string
-            byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(input);
-            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
-
-            // Hash the password with SHA256
-            bytesToBeEncrypted = SHA256.Create().ComputeHash(bytesToBeEncrypted);
-            passwordBytes = SHA256.Create().ComputeHash(passwordBytes);
-
-            byte[] bytesEncrypted = AES_Encrypt(bytesToBeEncrypted, passwordBytes);
-
-            string result = Convert.ToBase64String(bytesEncrypted);
-
-            return result;
-        }
+      
     }
 }
