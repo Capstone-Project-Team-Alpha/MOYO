62a6ab48316b8711d9aea5ed3c31d9929cbcfd36
sramji2 <102138307+sramji2@users.noreply.github.com>
2023-03-11 19:19:39 -0700

    Worked on Create Schedule

--- Model/Domain/MOYOHandler.cs
+++ Model/Domain/MOYOHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MOYO_Website.Technical_Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,18 @@ namespace MOYO_Website.Model.Domain
 {
     public class MOYOHandler
     {
+        #region Schedule
+
+        public bool CreateSchedule(string AppointmentDate, string AppointmentStartTime, string AppointmentEndTime, int StaffID, int MaxParticipants)
+        {
+            bool Confirmation = false;
+
+            Schedule ScheduleRequest = new Schedule();
+            Confirmation = ScheduleRequest.AddSchedule(AppointmentDate, AppointmentStartTime, AppointmentEndTime, StaffID, MaxParticipants);
+
+            return Confirmation;
+        }
+
+        #endregion
     }
 }
--- Model/Domain/Schedules.cs
+++ Model/Domain/Schedules.cs
@@ -1,6 +1,35 @@
-﻿namespace MOYO_Website.Model.Domain
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace MOYO_Website.Model.Domain
 {
     public class Schedules
     {
+        [BindProperty]
+        public int CalendarID { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Appointment Start Time is Required")]
+        public string AppointmentStartTime { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Appointment End Time is Required")]
+        public string AppointmentEndTime { get; set; }
+
+        [BindProperty]
+        [Required(ErrorMessage = "Appointment Date is Required")]
+        public string AppointmentDate { get; set; }
+
+        [BindProperty]
+        public string Message { get; set; }
+
+        [BindProperty]
+        public int StaffID { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "Appointment Name is Required")]
+        public string AppointmentName { get; set; }
+
+        [BindProperty]
+        public int MaxParticipants { get; set; }
     }
 }
